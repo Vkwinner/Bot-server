9947b6dd0fa3746c7df9a29e0cdf9da698df5012
Vkwinner <72137312+Vkwinner@users.noreply.github.com>
2024-01-23 21:12:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import fs from 'fs';
+// import bodyParser from 'body-parser';
+// import fs from 'fs';
 // import cors from 'cors';
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // app.use(cors());
 const port=4000;
 
-const botResponse=JSON.parse(fs.readFileSync('./data.json'))
+// const botResponse=JSON.parse(fs.readFileSync('./data.json'))
+ const botResponse=[
+    {
+        "id":1,
+        "content":"Hello"
+    },
+    {
+        "id":2,
+        "content":"How are You?"
+    },
+    {
+        "id":3,
+        "content":"How can i help you today?"
+    }
+]
 
 app.get('/',(req,res)=>{
     res.send(botResponse)
