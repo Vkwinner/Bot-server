ac023680a1703add42e2d4d2957a289d6bbf31e2
Vkwinner <vkbedi25@gmail.com>
2024-01-23 19:46:20 +0530

    edited index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import cors from 'cors';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port=4000;
+
+const botResponse=JSON.parse(fs.readFileSync('data.json'))
+
+app.get('/',(req,res)=>{
+    res.send(botResponse)
+})
+
+app.listen(port,()=>{
+    console.log(`server is listening on port http://localhost:${port}/ `);
+})
+
+export default app;
